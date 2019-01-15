661bd15e773a6d24844d1ad245de670da5465dfa
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2019-01-15 15:17:00 +0000

    Animator dude working

---
+++ Assets/PlatformerAssets/Scripts/Coin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    // Variable to let us add to score
+    //public so we can drag & drop
+//    public Score scoreObject;
+
+    // Variable to hold the coins point value 
+    //public so we can change in editor 
+    public int coinValue;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Unity calls this funcition when our coin touches any other object 
+    // If the player touches coin, score goes up
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if the thing we touched is the player
+        PlayerScript playerScript = collision.collider.GetComponent<PlayerScript>();
+
+        //if the thing we toyuched has a player script that means it is a player
+        if (playerScript)
+        {
+            //We hit the player
+            // Add to the score based on value
+ //           scoreObject.AddScore(coinValue);
+            //Destory the gameObject that is attached to the script 
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/PlatformerAssets/Scripts/PlayerScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+
+    // designer variables
+    public float speed = 10;
+    public float jumpSpeed = 10;
+    public string jumpButton = "jump";
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = "Horizontal";
+
+    public Animator playerAnimator;
+    public SpriteRenderer playerSprite;
+    public Collider2D playerCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Get axis input from Unity
+        float leftRight = Input.GetAxis(horizontalAxis);
+
+        // Create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight, 0);
+
+        // Make direction vector length 1
+        direction = direction.normalized;
+
+        // Calculate velocity
+        Vector2 velocity = direction * speed;
+        velocity.y = physicsBody.velocity.y;
+
+        // Give the velocity to the rigidbody
+        physicsBody.velocity = velocity;
+
+        //tell animator our speed
+
+        playerAnimator.SetFloat("walkSpeed", Mathf.Abs(velocity.x));
+        //flip our sprite if we're facing/moving backwards
+        if (velocity.x < 0)
+        {
+            playerSprite.flipX = true;
+
+        }
+
+        else
+        {
+
+            playerSprite.flipX = false;
+        }
+
+        LayerMask groundLayerMask = LayerMask.GetMask("Ground");
+        bool touchingGround = playerCollider.IsTouchingLayers(groundLayerMask);
+        bool jumpButtonPressed = Input.GetButtonDown(jumpButton);
+        if (jumpButtonPressed == true && touchingGround == true)
+        {
+            // We have pressed jump, so we should set our
+            // upward velocity to our jumpSpeed
+            velocity.y = jumpSpeed;
+
+            // Give the velocity to the rigidbody
+            physicsBody.velocity = velocity;
+        }
+
+    }
+}
+ 
+    
+
+
+
+
+
